d199a2806a3ccfb0e1217f8a80e46647843f53b9
Éderson Cássio <edersoncassiolf@outlook.com>
2023-03-10 21:07:44 -0300

    Lógica de negócio

--- internal/backend/backend.go
+++ internal/backend/backend.go
@@ -1,9 +1,45 @@
 package backend
 
+import (
+	"errors"
+
+	"com.blocopad/blocopad/db"
+)
+
+const tam_32k = 32*1024
+
 func GetKey(key string) (string, error) {
-	return "", nil
+	if len(key) == 0 || len(key) > 36 {
+		return "", errors.New("Tam. máx. da chave: 36")
+	}
+
+	oneTime, data, err := db.GetNote(key)
+
+	if err != nil {
+		return "", errors.New(err.Error())
+	}
+
+	if oneTime {
+		if err := db.DeleteNote(key); err != nil {
+			panic("Não foi possível deletar a nota de leitura única!")
+		}
+	}
+
+	return data, nil
 }
 
 func SaveKey(data string, onetime bool) (string, error) {
-	return "", nil
+	byteSize := len([]rune(data))
+
+	if byteSize == 0 || byteSize > tam_32k {
+		return "", errors.New("Tamanho de nota inválido")
+	}
+
+	uuidCode, err := db.SaveNote(data, onetime)
+
+	if err != nil {
+		return "", errors.New(err.Error())
+	}
+
+	return uuidCode, nil
 }
