91e83c7ed73b7d80ae4d09b23ce9d35290609262
Éderson Cássio <edersoncassiolf@outlook.com>
2023-04-10 15:12:50 -0300

    Testes da autenticação

--- it/integration_test.go
+++ it/integration_test.go
@@ -15,31 +15,83 @@ type SavedNote struct {
 	Code string `json:"code"`
 }
 
+type AccessToken struct {
+	AccessToken string
+}
+
 func TestSaveOK(t *testing.T) {
 	postUrl := "http://localhost:8080/api/note"
+	loginUrl := "http://localhost:8080/api/login"
 	jsonData := []byte(`{
 		"data": "eu deveria salvar isto?",
 		"onetime": false
 	}`)
-
-	request, _ := http.NewRequest("POST", postUrl, bytes.NewBuffer(jsonData))
-	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
-
+	credentialsData := []byte(`{
+		"username": "kânia",
+		"password": "búco"
+	}`)
 	client := &http.Client{}
-	response, err := client.Do(request)
+
+	// POST da nota sem autenticação
+	postRequest, _ := http.NewRequest("POST", postUrl, bytes.NewBuffer(jsonData))
+	postRequest.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	postResponse, err := client.Do(postRequest)
 
 	if err != nil {
 		panic(err)
 	}
 
-	defer response.Body.Close()
+	defer postResponse.Body.Close()
 
-	if response.StatusCode != http.StatusOK {
+	if postResponse.StatusCode != http.StatusForbidden {
+		t.Fatal("TestSaveOK deveria devolver 403 Forbidden")
+	}
+
+	// GET da nota sem autenticação
+	getRequest, _ := http.NewRequest("GET", postUrl+"/abc", nil)
+	getResponse, err := client.Do(getRequest)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer getResponse.Body.Close()
+
+	if getResponse.StatusCode != http.StatusForbidden {
+		t.Fatal("TestSaveOK deveria devolver 403 Forbidden")
+	}
+
+	// Autenticação :)
+	loginRequest, _ := http.NewRequest("POST", loginUrl, bytes.NewBuffer(credentialsData))
+	loginRequest.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	loginResponse, err := client.Do(loginRequest)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer loginResponse.Body.Close()
+	loginDecoder := json.NewDecoder(loginResponse.Body)
+	var token AccessToken
+
+	if err := loginDecoder.Decode(&token); err != nil {
+		panic(err)
+	}
+
+	// POST autenticado
+	postRequest.Header.Set("Authorization", "Bearer "+token.AccessToken)
+	postResponse, err = client.Do(postRequest)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if postResponse.StatusCode != http.StatusOK {
 		t.Fatal("TestSaveOK não deveria devolver erro")
 	}
 
 	var savedNote SavedNote
-	err = json.NewDecoder(response.Body).Decode(&savedNote)
+	err = json.NewDecoder(postResponse.Body).Decode(&savedNote)
 
 	if err != nil {
 		t.Fatal("TestSaveOK deveria devolver um JSON válido")
@@ -49,15 +101,17 @@ func TestSaveOK(t *testing.T) {
 		t.Fatal("TestSaveOK deveria devolver um UUID válido")
 	}
 
-	request2, _ := http.NewRequest("GET", postUrl+"/"+savedNote.Code, nil)
-	response2, err2 := client.Do(request2)
+	// GET autenticado
+	getRequest, _ = http.NewRequest("GET", postUrl+"/"+savedNote.Code, nil)
+	getRequest.Header.Set("Authorization", "Bearer "+token.AccessToken)
+	getResponse, err = client.Do(getRequest)
 
-	if err2 != nil {
-		panic(err2)
+	if err != nil {
+		panic(err)
 	}
 
-	defer response2.Body.Close()
-	binary, readError := io.ReadAll(response2.Body)
+	defer getResponse.Body.Close()
+	binary, readError := io.ReadAll(getResponse.Body)
 
 	if readError != nil {
 		panic(readError)
@@ -67,6 +121,7 @@ func TestSaveOK(t *testing.T) {
 		t.Fatal("TestSaveOK não recuperou a nota" + string(binary))
 	}
 
+	// Expiração da nota
 	rDB := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "",
